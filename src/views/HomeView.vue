f850bd2819c11863c39813382e8d2f45c6c08b83
Renault Kylian <kylian.renault@viacesi.fr>
2023-01-23 01:08:09 +0100

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,46 +1,18 @@
 <template>
-  <v-navigation-drawer
-    v-model="drawer"
-    app
-    clipped
-  >
-    <v-list dense>
-      <v-list-item-group>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
-  </v-navigation-drawer>
+  <HelloWorld />
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
+// Components
+import HelloWorld from '../components/HelloWorld.vue';
+
 export default defineComponent({
   name: 'HomeView',
 
-  data() {
-    return {
-      drawer: null,
-      items: [
-        { icon: 'mdi-home', title: 'Home' },
-        { icon: 'mdi-account', title: 'Profile' },
-        { icon: 'mdi-email', title: 'Messages' },
-        { icon: 'mdi-heart', title: 'Favorites' },
-        { icon: 'mdi-settings', title: 'Settings' },
-      ],
-    };
+  components: {
+    HelloWorld,
   },
 });
 </script>
