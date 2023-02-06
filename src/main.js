d5834668149da4ec7e88402c64833ddfdfcd1741
Renault Kylian <kylian.renault@viacesi.fr>
2023-02-07 00:26:49 +0100

    reparation of vuetify

--- src/main.js
+++ src/main.js
@@ -4,16 +4,7 @@ import router from './router'
 import CapacitorPlugin from './capacitor';
 //import './pusher-config'
 import i18n from './i18n.config'
-
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-  components,
-  directives,
-})
+import vuetify from './plugins/vuetify';
 
 
 
