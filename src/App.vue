2c5d2504be5d4bb45fc32b64185af8f14be6010c
Renault Kylian <kylian.renault@viacesi.fr>
2023-02-08 00:19:11 +0100

    add login and logout function

--- src/App.vue
+++ src/App.vue
@@ -40,13 +40,89 @@
         @click="this.is_light = !this.is_light;"
       ></v-btn>
 
-      <!-- button settings -->
-      <v-btn
-        v-if="is_Logged"
-        icon="mdi-cog"
-        to="/settings"
-      ></v-btn>
+      <!-- drawer menu -->
+      <v-app-bar-nav-icon
+        v-if="!is_Logged"
+        :icon="drawer ? 'mdi-menu-open' : 'mdi-menu'"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
     </v-app-bar>
+
+    <!-- drawer menu -->
+    <v-navigation-drawer
+      v-if="!is_Logged"
+      v-model="drawer"
+      location="right"
+    >
+      <!-- title with user avatar, pseudo and email -->
+      <v-list-item class="my-2">
+        <v-row>
+          <v-col>
+            <v-list-item-avatar>
+              <v-avatar size="50">
+                <v-img :src="user.avatar"></v-img>
+              </v-avatar>
+            </v-list-item-avatar>
+          </v-col>
+          <v-col>
+            <v-list-item-content>
+              <v-list-item-title>{{ user.pseudo }}</v-list-item-title>
+              <v-list-item-subtitle>{{ user.email }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-col>
+        </v-row>
+      </v-list-item>
+      <hr />
+      <v-list-item>
+        <v-btn
+          prepend-icon="mdi-view-dashboard"
+          href="/dashboard"
+          block
+          elevation="2"
+          size="x-large"
+        >
+          {{ $t('App.Drawer.Dashboard') }}
+        </v-btn>
+      </v-list-item>
+      <!-- friends -->
+      <v-list-item>
+        <v-btn
+          prepend-icon="mdi-account-multiple"
+          href="/friends"
+          block
+          elevation="2"
+          size="x-large"
+        >
+          {{ $t('App.Drawer.Friends') }}
+        </v-btn>
+      </v-list-item>
+      <!-- Groups -->
+      <v-list-item>
+        <v-btn
+          prepend-icon="mdi-account-group"
+          href="/groups"
+          block
+          elevation="2"
+          size="x-large"
+        >
+          {{ $t('App.Drawer.Groups') }}
+        </v-btn>
+      </v-list-item>
+      <hr />
+      <!-- settings -->
+      <v-list-item>
+        <v-btn
+          prepend-icon="mdi-cog"
+          href="/settings"
+          block
+          elevation="2"
+          size="x-large"
+        >
+          {{ $t('App.Drawer.Settings') }}
+        </v-btn>
+      </v-list-item>
+
+    </v-navigation-drawer>
 
     <v-main>
       <router-view />
@@ -59,24 +135,28 @@
     >
       <div class="d-flex justify-center align-center w-100">
         <!-- logo of github, linkedin and instagram in center-->
-        <v-icon
+        <v-btn
           class="mx-2"
-          color="white"
-          @click="window.open('https://github.com/Gammelinne')"
+          icon="mdi-github"
+          href='https://github.com/Gammelinne'
+          target="_blank"
         >
-          mdi-github
-        </v-icon>
-        <v-icon
+        </v-btn>
+        <v-btn
           class="mx-2"
-          color="white"
-          @click="window.open('https://www.linkedin.com/in/kylian-renault/')"
-        >
-          mdi-linkedin
-        </v-icon>
+          icon="mdi-linkedin"
+          href="https://www.linkedin.com/in/kylian-renault/"
+          target="_blank"
+        ></v-btn>
       </div>
-
+      <!--all link in center (about, contact, etc...) -->
+      <div class="d-flex justify-center align-center w-100 my-2">
+        <router-link to="/about" class="mx-2">{{ $t('App.Footer.About') }}</router-link>
+        <router-link to="/contact" class="mx-2">{{ $t('App.Footer.Contact') }}</router-link>
+        <router-link to="/legal" class="mx-2">{{ $t('App.Footer.Legal') }}</router-link>
+      </div>
       <div class="px-4 py-2 text-center w-100">
-        {{ new Date().getFullYear() }} — <span>{{$t('App.Footer')}}</span>
+        <span>{{$t('App.Footer.Description')}} — {{$t('App.Title')}} &copy; {{ new Date().getFullYear() }}</span>
       </div>
     </v-footer>
 
@@ -93,6 +173,11 @@ export default {
     is_light: true,
     is_Logged: false,
     language: null,
+    user: {
+      pseudo: "Gameline",
+      email: "gameline@lahyra.com",
+      avatar: "https://avatars.githubusercontent.com/u/56442535?v=4",
+    },
   }),
   created: function () {
     this.is_Logged = setAuthorizationHeader();
